ad17f483213c5d47b4356138deaf79a9b2fc3dae
Tlochers <tom.f.loughlin@gmail.com>
2016-01-26 19:41:47 +0000

    colour change and structure of javascript

--- js/application.js
+++ js/application.js
@@ -5,4 +5,20 @@
    // your page initialization code here
    // the DOM will be available here
 
+window.onload = init;
+function init(){
+
+  document.getElementById("next").onclick = addMyClass();
+}
+
+
+function addMyClass() {
+
+  var select = document.getElementById("test");
+
+  var classString = select.className; // returns the string of all the classes for myDiv
+  var newClass = classString.concat(" animated zoomInLeft"); // Adds the class "main__section" to the string (notice the leading space)
+  select.className = newClass; // sets className to the new string
+}
+
 })();
