8c2412214b70d59d40938279824199045b6ddc6c
Tlochers <tom.f.loughlin@gmail.com>
2016-01-31 14:48:57 +0000

    parent node and child divs

--- js/application.js
+++ js/application.js
@@ -8,17 +8,40 @@
 window.onload = init;
 function init(){
 
-  document.getElementById("next").onclick = addMyClass();
+  document.getElementById('next').addEventListener('click', addMovementStates);
+
+  // document.getElementById("next").onclick = addMovementStates();
 }
 
 
-function addMyClass() {
+function addMovementStates() {
+  var children = document.getElementById('main-textarea').childNodes;
 
-  var select = document.getElementById("test");
+  _.each(children, function(singleNode) {
+    if(singleNode.nodeName === 'DIV') {
+      console.log(singleNode);
+    }
+  });
+  console.log(children)
+  // var el = document.getElementById('div-01').nextSibling,
+  // findNextDiv();
+  // exitAnimation();
+  // var select = document.getElementById("test");
 
-  var classString = select.className; // returns the string of all the classes for myDiv
-  var newClass = classString.concat(" animated zoomInLeft"); // Adds the class "main__section" to the string (notice the leading space)
-  select.className = newClass; // sets className to the new string
+  // var classString = select.className; // returns the string of all the classes for myDiv
+  // var newClass = classString.concat(" animated zoomInLeft"); // Adds the class "main__section" to the string (notice the leading space)
+  // select.className = newClass; // sets className to the new string
 }
 
+function findNextDiv(){
+  // var select = document.getElementById("test");
+  var selected =  document.getElementsByClassName("selected");
+  console.log(selected.nextSibling)
+  var classString = selected.className;
+  selected.className = newClass;
+
+}
+function exitAnimation(){
+  
+}
 })();
