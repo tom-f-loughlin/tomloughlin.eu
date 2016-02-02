6f00fa539527a3b59b603008d331a2c6492b1647
Tlochers <tom.f.loughlin@gmail.com>
2016-02-02 20:43:48 +0000

    skeleton for adding/removing classed based on only knowing parent div for carosel

--- js/application.js
+++ js/application.js
@@ -2,69 +2,70 @@
 
 
 (function() {
-   // your page initialization code here
-   // the DOM will be available here
 
+// your page initialization code here
 window.onload = init;
-function init(){
 
+//Variables
+var selectedValue = 'selected';
+
+function init(){
   document.getElementById('next').addEventListener('click', findNextDiv);
   document.getElementById('prev').addEventListener('click', findPrevDiv);
-
   // document.getElementById("next").onclick = addMovementStates();
 }
 
 
 function findPrevDiv() {
-  var children = document.getElementById('main-textarea').childNodes;
+    children = document.getElementById('main-textarea').childNodes;
 
-
-  var test = _.pluck(children, 'div.text-obj');
-  console.log(test);
-  // _.each(children, function(singleNode) {
-  //   if(singleNode.nodeName === 'DIV') {
-  //     console.log(singleNode);
-  //   }
-  // });
-  console.log(children)
+  _.each(children, function(singleNode) {
+    if(singleNode.nodeName === 'DIV') {
+      var theOneName = singleNode.className;
+      if(theOneName.indexOf(selectedValue) > -1){
+        console.log(singleNode.previousSibling);
+        var removed = singleNode.className.replace('selected','');
+        singleNode.className = removed;
+      }
+    }
+  });
+  console.log(children);
   // var el = document.getElementById('div-01').nextSibling,
   // findNextDiv();
   // exitAnimation();
   // var select = document.getElementById("test");
 
   // var classString = select.className; // returns the string of all the classes for myDiv
+  // console.log(classString);
   // var newClass = classString.concat(" animated zoomInLeft"); // Adds the class "main__section" to the string (notice the leading space)
   // select.className = newClass; // sets className to the new string
 }
-var testingArray  = [];
 
-function findNextDiv(){
-  // var select = document.getElementById("test");
-    var children = document.getElementById('main-textarea').childNodes;
-    var selected = document.getElementsByClassName('selected');
-    console.log(selected);
-  
-    var nextObj = selected[0].nextElementSibling;
+function findNextDiv(){  
+    children = document.getElementById('main-textarea').childNodes;
 
-    console.log(nextObj);
+  _.each(children, function(singleNode) {
+    if(singleNode.nodeName === 'DIV') {
+      console.log(singleNode.className);
+      var theOneName = singleNode.className;
+      if(theOneName.indexOf(selectedValue) > -1){
+        console.log('here',singleNode.nextSibling.nextSibling);
+        var removed = singleNode.className.replace('selected','selected prevleaving');
+        // var nextDiv = singleNode.nextSibling.nextSibling;
+        // console.log('what im looking for',singleNode);
+        // var newClass = nextDiv.className.concat(" selected next"); 
+        // nextDiv.className = newClass;
+
+        singleNode.className = removed;
+      }
+    }
+  });
 
     // unable to use the proto unless dom is added to array
     // for (i = 0; i < children.length; i++) { 
     //    console.log('dividing the children', children[i]);
     // }
 
-//     _.each(children, function(singleNode) {
-//     if(singleNode.nodeName === 'DIV') {
-//         console.log(singleNode);
-
-//         testingArray.push(singleNode);
-//       }
-//     });
-// console.log(testingArray);
-  // var selected =  document.getElementsByClassName("selected");
-  // console.log(selected.nextSibling)
-  // var classString = selected.className;
-  // selected.className = newClass;
 
 }
 function exitAnimation(){
